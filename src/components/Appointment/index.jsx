46f4f43d520e5b5e5928742d9ca8d4fd4faadfc2
Meghal <meghal.shah210@gmail.com>
2021-10-29 21:42:19 -0400

    Building a custom hook

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -3,9 +3,13 @@ import "./styles.scss";
 import Header from "./Header.jsx";
 import Empty from "./Empty.jsx";
 import Show from "./Show.jsx";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
-  const appointmentWithTime = "Appointment at " + props.time;
+  // const appointmentWithTime = "Appointment at " + props.time;
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+
   return (
     <article className="appointment">
       <Header id={props.id} time={props.time} />
