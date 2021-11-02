95182c109b431c5a33eb18b3ee5d31ec04a426a9
Meghal <meghal.shah210@gmail.com>
2021-11-02 17:02:58 -0400

    Test form without student name shows error

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 
 import Form from "components/Appointment/Form";
 
@@ -31,21 +31,30 @@ describe("Form", () => {
   });
 
   it("validates that the student name is not blank", () => {
-    /* 1. validation is shown */
+    const onSave = jest.fn();
+    const { getByText } = render(
+      <Form interviewers={interviewers} onSave={onSave} />
+    );
+    fireEvent.click(getByText("Save"));
+
     expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
-  
-    /* 2. onSave is not called */
+
     expect(onSave).not.toHaveBeenCalled();
   });
   
   it("calls onSave function when the name is defined", () => {
-    /* 3. validation is not shown */
+    const onSave = jest.fn();
+
+    const { getByText, queryByText } = render(
+      <Form interviewers={interviewers} student='Lydia Miller-Jones' onSave={onSave}/>
+    );
+
+    fireEvent.click(getByText("Save"));
+
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
   
-    /* 4. onSave is called once*/
     expect(onSave).toHaveBeenCalledTimes(1);
-  
-    /* 5. onSave is called with the correct arguments */
+
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
   });
 });
