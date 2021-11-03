90eb4a12441fa15f5374079cb889a6a4b0eb2d68
Meghal <meghal.shah210@gmail.com>
2021-11-03 18:22:50 -0400

    Test for edit an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -28,4 +28,26 @@ describe("Appointment", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
+
+  it("should edit an interview", () => {
+    cy.get(".appointment__card")
+      .first()
+      .should('be.visible');
+
+    cy.get('[alt="Edit"]')
+      .click({ force: true });
+
+    cy.get('[data-testid="student-name-input"]')
+      .clear()
+      .type("Lydia Miller-Jones")
+
+    cy.get('[alt="Tori Malcolm"]')
+      .click();
+
+    cy.contains("Save")
+      .click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
 })
