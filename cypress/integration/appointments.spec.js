6291742a1e2a65b105ed85360ffc9b5de9ce2805
Meghal <meghal.shah210@gmail.com>
2021-11-03 17:36:00 -0400

    Test for Book an interview

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,31 @@
+// appointments.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+describe("Appointment", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+    cy.visit("/")
+      .contains("[data-testid=day]", "Monday");
+  })
+
+  it("should book an interview", () => {
+    cy.get('[alt="Add"]')
+      .first()
+      .click();
+
+    cy.get('[data-testid="student-name-input"]')
+      .type("Lydia Miller-Jones")
+
+    cy.get('[alt="Sylvia Palmer"]')
+      .click();
+
+    cy.contains("Save")
+      .click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+})
